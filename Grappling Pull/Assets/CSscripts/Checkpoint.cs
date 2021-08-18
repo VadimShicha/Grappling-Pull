8b84097f262a37c68f49a0fc4a8482f24c1e7899
VadimShicha <shichavadim@gmail.com>
2021-08-17 23:48:02 -0700

    Checkpoints and Prefabs
    Checkpoints and Prefabs

---
+++ Grappling Pull/Assets/CSscripts/Checkpoint.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+	public int checkpointNumber = 0;
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision == Main.playerInstacne.GetComponent<Collider2D>())
+		{
+			setCheckpoint();
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject == Main.playerInstacne)
+		{
+			setCheckpoint();
+		}
+	}
+
+	void setCheckpoint()
+	{
+		if(checkpointNumber > VarManager.checkpointNumber)
+		{
+			VarManager.checkpointPos = gameObject.transform.position;
+			VarManager.checkpointNumber = checkpointNumber;
+		}
+	}
+
+}
--- Grappling Pull/Assets/CSscripts/VarManager.cs
+++ Grappling Pull/Assets/CSscripts/VarManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class VarManager : MonoBehaviour
 {
+    //used to make sure if you go on an old checkpoint the checkpoint position doesn't change
+    public static int checkpointNumber;
     public static Vector3 checkpointPos;
     public static bool respawned;
 }
